ed6d73d1963d474cf8c781c63830f008e310f9f0
Roland Rodriguez <roland@rodzil.la>
2019-01-12 03:00:27 -0800

    beginning geo seach for donor code

--- searchForDonors/index.js
+++ searchForDonors/index.js
@@ -9,55 +9,98 @@ AWS
     .config
     .update({region: "us-west-1"});
 
+async function publishSNSMessage(message, event_type) {
+
+    var params = {
+        Message: JSON.stringify(message),
+        /* required */
+        TopicArn: process.env.SNS_TOPIC,
+        MessageAttributes: {
+            "event.type": {
+                DataType: "String",
+                StringValue: event_type
+            }
+        }
+    };
+
+    // Create promise and SNS service object
+    try {
+        var publishTextPromise = new AWS
+            .SNS({apiVersion: '2010-03-31'})
+            .publish(params)
+            .promise();
+
+        await publishTextPromise.then(function (data) {
+            console.log(`Message ${params.Message} send sent to the topic ${params.TopicArn}`);
+            console.log("Message " + data);
+            var jsonData = JSON.parse(params.Message);
+
+            console.log("json data:");
+            console.log(jsonData);
+            return;
+        })
+            .catch(function (err) {
+                console.error(err, err.stack);
+                return err;
+            });
+
+    } catch (err) {
+        return err;
+    }
+
+}
+
 exports.handler = async(event) => {
 
-    //in this function we need ot do a geo search based on the incoming latitude/longitudes
-    //of the donor.  then we need to either send out no donors found message or a donors found
-    //along with a list of the donor phone numbers so we can alert them later.  i believe we need
-    //to save that list of donors to the request record so we know who we contacted later.  we may
-    //need that list in order to send updates and cancellation notifications
+    // in this function we need ot do a geo search based on the incoming
+    // latitude/longitudes of the donor.  then we need to either send out no donors
+    // found message or a donors found along with a list of the donor phone numbers
+    // so we can alert them later.  i believe we need to save that list of donors
+    // to the request record so we know who we contacted later.  we may need that
+    // list in order to send updates and cancellation notifications let's parse this
+    // incoming record
+    var record = JSON.parse(event.Records[0].body);
+    console.log("INCOMING RECORD: ");
+    console.log(record);
+    console.log('event.type');
+    //event type should be open.new.request
+    let event_type = record.MessageAttributes["event.type"].Value;
+    console.log("INCOMING RECORD.MESSAGE: ");
+    console.log("EVENT TYPE: " + event_type);
 
-    let entity = event.entity;
-    let entityKey = event.key;
-    let entityLocation = JSON.parse(event.location);
-    let params = JSON.parse(event.params);
+    //need to parse twice since the json gets escaped twice
+    let msg_org = JSON.parse(record.Message);
+    console.log("SUCCESS PARSE 1");
+    console.log(msg_org);
+    let msg_obj = JSON.parse(msg_org);
+    console.log("SUCCESS PARSE 2");
+
+    console.log(msg_obj);
+
+    let lat = parseFloat(msg_org.params.request.lat);
+    let lon = parseFloat(msg_org.params.request.lon);
+
+    let msg = event.message;
+    console.log(msg);
+
+    // let entity = event.entity; let entityKey = event.key; let entityLocation =
+    // JSON.parse(event.location); let params = JSON.parse(event.params);
 
     const ddb = new AWS.DynamoDB({endpoint: "dynamodb.us-west-1.amazonaws.com"});
-    const config = new ddbGeo.GeoDataManagerConfiguration(ddb, entity);
+    const config = new ddbGeo.GeoDataManagerConfiguration(ddb, "volunteers");
     const myGeoTableManager = new ddbGeo.GeoDataManager(config);
 
     // Querying 100km from Cambridge, UK
     myGeoTableManager.queryRadius({
-        RadiusInMeter: 100000,
+        RadiusInMeter: 5000,
         CenterPoint: {
-            latitude: 52.225730,
-            longitude: 0.149593
+            latitude: lat,
+            longitude: lon
         }
     })
     // Print the results, an array of DynamoDB.AttributeMaps
-        .then(console.log);
+        .then((results) => {
+        console.log(results);
+    });
 
-    myGeoTableManager.putPoint({
-        RangeKeyValue: {
-            S: entityKey
-        }, // Use this to ensure uniqueness of the hash/range pairs.
-        GeoPoint: { // An object specifying latitutde and longitude as plain numbers. Used to build the geohash, the hashkey and geojson data
-            latitude: entityLocation.lat,
-            longitude: entityLocation.lon
-        },
-            PutItemInput: { // Passed through to the underlying DynamoDB.putItem request. TableName is filled in for you.
-                Item: params
-            }
-        })
-        .promise()
-        .then(function () {
-
-            callback(null, null);
-
-        })
-        .catch((error) => {
-            console.log("FAIL:");
-            console.log(error);
-            callback(error, null);
-        });
 };
