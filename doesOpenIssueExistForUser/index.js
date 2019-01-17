9dcd2e1fb55cd0c35ff0f235d691a203be6e57ec
Roland Rodriguez <roland@rodzil.la>
2019-01-16 19:25:19 -0800

    added ability to iterate multiple records for batch handling

--- doesOpenIssueExistForUser/index.js
+++ doesOpenIssueExistForUser/index.js
@@ -58,64 +58,65 @@ async function asyncForEach(array, callback) {
     }
 }
 
-
 exports.handler = async(event) => {
 
-    
+    await asyncForEach(event.Records, async(record) => {
+        //let's parse this incoming record
+        let message = JSON.parse(record.body);
+        let event_type = record.messageAttributes["event.type"].stringValue;
+        let flow_sid = record.messageAttributes["event.flow"].stringValue;
 
-    //let's parse this incoming record
-    let message = JSON.parse(event.Records[0].body);
-    let event_type = event.Records[0].messageAttributes["event.type"].stringValue;
-    let flow_sid = event.Records[0].messageAttributes["event.flow"].stringValue;
+        console.log("INCOMING RECORD.MESSAGE: ");
+        console.log("EVENT TYPE: " + event_type);
+        //need to parse twice since the json gets escaped twice
 
-    console.log("INCOMING RECORD.MESSAGE: ");
-    console.log("EVENT TYPE: " + event_type);
-    //need to parse twice since the json gets escaped twice
+        console.log("message: ");
+        console.log(message);
 
-    console.log("message: ");
-    console.log(message);
+        //we want to check the database for an existing open issue for this user
 
-    //we want to check the database for an existing open issue for this user
+        var docClient = new AWS
+            .DynamoDB
+            .DocumentClient({endpoint: "dynamodb.us-west-1.amazonaws.com"});
 
-    var docClient = new AWS
-        .DynamoDB
-        .DocumentClient({endpoint: "dynamodb.us-west-1.amazonaws.com"});
+        var params = {
+            TableName: "need_request",
+            IndexName: "phone_number-status-index",
+            KeyConditionExpression: "phone_number = :phone_number AND #request_status = :status ",
+            ExpressionAttributeValues: {
+                ":phone_number": message.to,
+                ":status": "open"
+            },
+            ExpressionAttributeNames: {
+                "#request_status": "status"
+            }
+        };
 
-    var params = {
-        TableName: "need_request",
-        IndexName: "phone_number-status-index",
-        KeyConditionExpression: "phone_number = :phone_number AND #request_status = :status ",
-        ExpressionAttributeValues: {
-            ":phone_number": message.to,
-            ":status": "open"
-        },
-        ExpressionAttributeNames: {
-            "#request_status": "status"
-        }
-    };
+        let queryPromise = docClient
+            .query(params)
+            .promise();
 
-    let queryPromise = docClient
-        .query(params)
-        .promise();
+        await queryPromise.then(async(results) => {
+            //then send a message based on whether something was found or not
+            await publishSNSMessage({
+                "from": message.from,
+                "to": message.to,
+                "params": (results.Items.length === 0)
+                    ? {
+                        "type": "open.issue.not.found"
+                    }
+                    : {
+                        "type": "open.issue.found",
+                        "request": results.Items[0]
+                    }
+            }, (results.Items.length === 0)
+                ? "open.issue.not.found"
+                : "open.issue.found", flow_sid).then((data) => {
+                return;
+            });
 
-    await queryPromise.then(async(results) => {
-        //then send a message based on whether something was found or not
-        await publishSNSMessage({
-            "from": message.from,
-            "to": message.to,
-            "params": (results.Items.length === 0)
-                ? {
-                    "type": "open.issue.not.found"
-                }
-                : {
-                    "type": "open.issue.found",
-                    "request": results.Items[0]
-                }
-        }, (results.Items.length === 0)
-            ? "open.issue.not.found"
-            : "open.issue.found", flow_sid).then((data) => {
-            return;
         });
 
-    })
+    });
+
 };
