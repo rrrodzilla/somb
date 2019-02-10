8704d177c4d6c5a92f2a54947285d656ec5a8db6
Roland Rodriguez <roland@rodzil.la>
2019-02-10 12:52:35 -0800

    refactored initial account check

--- handleDonorCommand/index.js
+++ handleDonorCommand/index.js
@@ -78,7 +78,7 @@ async function updateStatusMsg(from, to, request_obj, status, flow_sid) {
             "timestamp": request_obj.timestamp
         },
         "params": {
-            "status": status,
+            "fulfillment_status": status,
             "donor": (request_obj.donor)
                 ? request_obj.donor
                 : {},
@@ -155,109 +155,101 @@ exports.handler = async(event) => {
             } else {
                 let need_request = results.Items[0];
                 console.log(need_request);
+                var response_message = "";
+
+                switch (need_request.status) {
+                    case STATUS_CANCELED:
+                        response_message = CANCELED_REQUEST_RESPONSE;
+                        break;
+                    case STATUS_EXPIRED:
+                        response_message = EXPIRED_REQUEST_RESPONSE;
+                        break;
+                    case STATUS_OPEN:
+                        switch (message.params.command) {
+                            case "give":
+                                switch (need_request.fulfillment_status) {
+                                    case STATUS_OPEN:
+                                        need_request.donor = message.to;
+                                        await updateStatusMsg(message.from, message.to, need_request, STATUS_DONOR_FOUND, flow_sid).then(() => {
+                                            return;
+                                        });
+                                        //updated_status = STATUS_DONOR_FOUND; //and add donor to request record
+                                        break;
+                                    case STATUS_DONOR_FOUND:
+                                        response_message = DONOR_EXISTS_RESPONSE;
+                                        break;
+                                    case STATUS_DELIVERER_FOUND:
+                                        need_request.donor = message.to;
+                                        await updateStatusMsg(message.from, message.to, need_request, STATUS_AWAITING_DELIVERY, flow_sid).then(() => {
+                                            return;
+                                        });
+                                        //updated_status = STATUS_AWAITING_DELIVERY; //and add donor to request record
+                                        break;
+                                    case STATUS_AWAITING_DELIVERY:
+                                        response_message = AWAITING_DELIVERY_RESPONSE;
+                                        break;
+                                    case STATUS_FULFILLED:
+                                        response_message = ALREADY_FULFILLED_RESPONSE;
+                                        break;
+                                }
+                                break;
+                            case "deliver":
+                                switch (need_request.fulfillment_status) {
+                                    case STATUS_OPEN:
+                                        need_request.deliverer = message.to;
+                                        await updateStatusMsg(message.from, message.to, need_request, STATUS_DELIVERER_FOUND, flow_sid).then(() => {
+                                            return;
+                                        });
+                                        // updated_status = STATUS_DELIVERER_FOUND; //and add donor to request record
+                                        break;
+                                    case STATUS_DONOR_FOUND:
+                                        need_request.deliverer = message.to;
+                                        await updateStatusMsg(message.from, message.to, need_request, STATUS_AWAITING_DELIVERY, flow_sid).then(() => {
+                                            return;
+                                        });
+                                        // updated_status = STATUS_AWAITING_DELIVERY; //and add donor to request record
+                                        break;
+                                    case STATUS_DELIVERER_FOUND:
+                                        response_message = DELIVERER_EXISTS_RESPONSE;
+                                        break;
+                                    case STATUS_AWAITING_DELIVERY:
+                                        response_message = AWAITING_DELIVERY_RESPONSE;
+                                        break;
+                                    case STATUS_FULFILLED:
+                                        response_message = ALREADY_FULFILLED_RESPONSE;
+                                        break;
+                                }
+                                break;
+                            case "provide":
+                                switch (need_request.fulfillment_status) {
+                                    case STATUS_OPEN:
+                                        need_request.deliverer = message.to;
+                                        need_request.donor = message.to;
+                                        await updateStatusMsg(message.from, message.to, need_request, STATUS_AWAITING_DELIVERY, flow_sid).then(() => {
+                                            return;
+                                        });
+                                        // updated_status = STATUS_AWAITING_DELIVERY; //and add donor to request record
+                                        break;
+                                    case STATUS_DONOR_FOUND:
+                                        response_message = DONOR_EXISTS_RESPONSE;
+                                        break;
+                                    case STATUS_DELIVERER_FOUND:
+                                        response_message = DELIVERER_EXISTS_RESPONSE;
+                                        break;
+                                    case STATUS_AWAITING_DELIVERY:
+                                        response_message = AWAITING_DELIVERY_RESPONSE;
+                                        break;
+                                    case STATUS_FULFILLED:
+                                        response_message = ALREADY_FULFILLED_RESPONSE;
+                                        break;
+                                }
+                                break;
+
+                        }
+                }
 
                 //let's see what command we have and then send he appropriate status update
 
-                var response_message = "";
-                switch (message.params.command) {
-                    case "give":
-                        switch (need_request.status) {
-                            case STATUS_CANCELED:
-                                response_message = CANCELED_REQUEST_RESPONSE;
-                                break;
-                            case STATUS_EXPIRED:
-                                response_message = EXPIRED_REQUEST_RESPONSE;
-                                break;
-                            case STATUS_OPEN:
-                                need_request.donor = message.to;
-                                await updateStatusMsg(message.from, message.to, need_request, STATUS_DONOR_FOUND, flow_sid).then(() => {
-                                    return;
-                                });
-                                //updated_status = STATUS_DONOR_FOUND; //and add donor to request record
-                                break;
-                            case STATUS_DONOR_FOUND:
-                                response_message = DONOR_EXISTS_RESPONSE;
-                                break;
-                            case STATUS_DELIVERER_FOUND:
-                                need_request.donor = message.to;
-                                await updateStatusMsg(message.from, message.to, need_request, STATUS_AWAITING_DELIVERY, flow_sid).then(() => {
-                                    return;
-                                });
-                                //updated_status = STATUS_AWAITING_DELIVERY; //and add donor to request record
-                                break;
-                            case STATUS_AWAITING_DELIVERY:
-                                response_message = AWAITING_DELIVERY_RESPONSE;
-                                break;
-                            case STATUS_FULFILLED:
-                                response_message = ALREADY_FULFILLED_RESPONSE;
-                                break;
-                        }
-                        break;
-                    case "deliver":
-                        switch (need_request.status) {
-                            case STATUS_CANCELED:
-                                response_message = CANCELED_REQUEST_RESPONSE;
-                                break;
-                            case STATUS_EXPIRED:
-                                response_message = EXPIRED_REQUEST_RESPONSE;
-                                break;
-                            case STATUS_OPEN:
-                                need_request.deliverer = message.to;
-                                await updateStatusMsg(message.from, message.to, need_request, STATUS_DELIVERER_FOUND, flow_sid).then(() => {
-                                    return;
-                                });
-                                // updated_status = STATUS_DELIVERER_FOUND; //and add donor to request record
-                                break;
-                            case STATUS_DONOR_FOUND:
-                                need_request.deliverer = message.to;
-                                await updateStatusMsg(message.from, message.to, need_request, STATUS_AWAITING_DELIVERY, flow_sid).then(() => {
-                                    return;
-                                });
-                                // updated_status = STATUS_AWAITING_DELIVERY; //and add donor to request record
-                                break;
-                            case STATUS_DELIVERER_FOUND:
-                                response_message = DELIVERER_EXISTS_RESPONSE;
-                                break;
-                            case STATUS_AWAITING_DELIVERY:
-                                response_message = AWAITING_DELIVERY_RESPONSE;
-                                break;
-                            case STATUS_FULFILLED:
-                                response_message = ALREADY_FULFILLED_RESPONSE;
-                                break;
-                        }
-                        break;
-                    case "provide":
-                        switch (need_request.status) {
-                            case STATUS_CANCELED:
-                                response_message = CANCELED_REQUEST_RESPONSE;
-                                break;
-                            case STATUS_EXPIRED:
-                                response_message = EXPIRED_REQUEST_RESPONSE;
-                                break;
-                            case STATUS_OPEN:
-                                need_request.deliverer = message.to;
-                                need_request.donor = message.to;
-                                await updateStatusMsg(message.from, message.to, need_request, STATUS_AWAITING_DELIVERY, flow_sid).then(() => {
-                                    return;
-                                });
-                                // updated_status = STATUS_AWAITING_DELIVERY; //and add donor to request record
-                                break;
-                            case STATUS_DONOR_FOUND:
-                                response_message = DONOR_EXISTS_RESPONSE;
-                                break;
-                            case STATUS_DELIVERER_FOUND:
-                                response_message = DELIVERER_EXISTS_RESPONSE;
-                                break;
-                            case STATUS_AWAITING_DELIVERY:
-                                response_message = AWAITING_DELIVERY_RESPONSE;
-                                break;
-                            case STATUS_FULFILLED:
-                                response_message = ALREADY_FULFILLED_RESPONSE;
-                                break;
-                        }
-                        break;
-
-                }
                 if (response_message !== "") {
                     await broadcastMsg(message.from, message.to, message.params.request_id, response_message, flow_sid).then(() => {
                         return;
